8065bcace431589dc0bf99697293fef31bf42141
Reinier Hernández <sasuke.reinier@gmail.com>
2024-07-13 14:54:35 -0300

    refactor(cli): remove redundant imports and use fs-extra instead

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,6 @@
 //  \____/\____/ |_| |_| |_| \__,_||_|    \__| CLI
 //  Created by: Reinier Hernández
 
-import { readFileSync } from "fs";
-import { dirname, join } from "path";
 import { Command } from "commander";
 import commands from "./gsmart";
 
@@ -19,16 +17,24 @@ process.on('SIGINT', handleSigTerm)
 process.on('SIGTERM', handleSigTerm)
 
 // Define the program
-const __dirname = dirname(".");
 const program = new Command();
-const packageJson = JSON.parse(
-  readFileSync(join(__dirname, "/package.json"), "utf-8")
-);
+
+import path from "path"
+import fs from "fs-extra"
+import { type PackageJson } from "type-fest"
+
+function getPackageInfo() {
+  const packageJsonPath = path.join("package.json")
+
+  return fs.readJSONSync(packageJsonPath) as PackageJson
+}
+
+const packageJson = getPackageInfo();
 
 program
-  .name(packageJson.name)
-  .version(packageJson.version)
-  .description(packageJson.description);
+  .name(packageJson.name || "gsmart")
+  .version(packageJson.version || "latest")
+  .description(packageJson.description || "GSmart CLI");
 
 for (const command of commands) {
   const cmd = program
