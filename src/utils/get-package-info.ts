1bfa992be781529c4a93d21ea44bf2770ccfe7a4
Reinier Hernández <sasuke.reinier@gmail.com>
2024-07-13 15:16:56 -0300

    Update version to 0.5.2 (#12)

--- src/utils/get-package-info.ts
+++ src/utils/get-package-info.ts
@@ -1,9 +1,14 @@
-import path from "path"
-import fs from "fs-extra"
-import { type PackageJson } from "type-fest"
+import path from "path";
+import fs from "fs-extra";
+import { type PackageJson } from "type-fest";
 
 export function getPackageInfo() {
-  const packageJsonPath = path.join("package.json")
+  // const packageJsonPath = path.join("package.json");
 
-  return fs.readJSONSync(packageJsonPath) as PackageJson
+  // return fs.readJSONSync(packageJsonPath) as PackageJson;
+  return {
+    name: "gsmart",
+    version: "0.5.2",
+    description: "CLI to generate smart commit messages using AI.",
+  }
 }
