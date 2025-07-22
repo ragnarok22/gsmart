bb79f8ef6bd3ceb5a3f5928436c3a74fc1d88a58
Reinier Hernández <sasuke.reinier@gmail.com>
2025-07-22 09:36:57 -0300

    feat(lint): integrate Prettier with ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   {
@@ -21,4 +22,5 @@ export default defineConfig([
     language: "json/json",
     extends: ["json/recommended"],
   },
+  eslintConfigPrettier,
 ]);
